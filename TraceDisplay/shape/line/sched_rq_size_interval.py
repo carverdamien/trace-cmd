90bb26c185bb5b3c02d76951f1c5a2720facf307
Damien Carver <carverdamien@gmail.com>
2020-01-31 16:06:27 +0100

    fix

--- TraceDisplay/shape/line/sched_rq_size_interval.py
+++ TraceDisplay/shape/line/sched_rq_size_interval.py
@@ -3,12 +3,12 @@ def sched_rq_size_interval(df):
     df = df['sched_rq_size_change']
     size = np.array(df['size'])
     timestamp = np.array(df.index)
+    next_timestamp_on_same_cpu = np.array(df.index)
     cpu = np.array(df['cpu'])
-    next_timestamp_on_same_cpu = np.array(timestamp)
     idx = np.arange(len(next_timestamp_on_same_cpu))
     # TODO: in parallel
     for i in np.unique(cpu):
-        sel = cpu & i
+        sel = cpu == i
         next_timestamp_on_same_cpu[idx[sel][:-1]] = next_timestamp_on_same_cpu[idx[sel][1:]]
     return {
         'x0' : timestamp,
