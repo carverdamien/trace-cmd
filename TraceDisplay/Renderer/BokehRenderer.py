8794011c4fd41c98e40931792b254de870c9e28b
Damien Carver <carverdamien@gmail.com>
2020-02-03 16:18:08 +0100

    BokehRenderer: add dummy datashader

--- TraceDisplay/Renderer/BokehRenderer.py
+++ TraceDisplay/Renderer/BokehRenderer.py
@@ -1,8 +1,10 @@
 from bokeh.plotting import figure
 from bokeh.io import show, push_notebook, output_notebook
-from bokeh.models import CustomJS
-from datashader.bokeh_ext import InteractiveImage
+from bokeh.models import CustomJS, ColumnDataSource
 from ipywidgets import interact_manual
+import datashader as ds
+import datashader.transfer_functions as tf
+import pandas as pd
 
 output_notebook()
 
@@ -38,7 +40,56 @@ class BokehRenderer(object):
         self.figure.x_range.callback = CustomJS(code=FIGURE_RANGE_JSCODE % (self.brid, 'x'))
         self.figure.y_range.callback = CustomJS(code=FIGURE_RANGE_JSCODE % (self.brid, 'y'))
         self.notebook_handle = None
+        df = pd.DataFrame({
+            'x0' : [0],
+            'x1' : [1],
+            'y0' : [0],
+            'y1' : [1],
+            'c'  : [0],
+        })
+        df['c'] = df['c'].astype('category')
+        self.lines = df
+        self.color_key = ['#000000']
+        image = self.updateImage()
+        xmin, xmax = self.figure.x_range.start, self.figure.x_range.end
+        ymin, ymax = self.figure.y_range.start, self.figure.y_range.end
+        x_range = (xmin, xmax)
+        y_range = (ymin, ymax)
+        dw, dh = xmax - xmin, ymax - ymin
+        self.source = ColumnDataSource(data=dict(image=[image.data], x=[xmin],
+                                                 y=[ymin], dw=[dw], dh=[dh]))
+        self.renderer = self.figure.image_rgba(source=self.source,
+                                               image='image',
+                                               x='x',
+                                               y='y',
+                                               dw='dw',
+                                               dh='dh',
+                                               dilate=False,
+        )
         pass
+
+    def updateImage(self):
+        plot_width, plot_height = self.figure.plot_width, self.figure.plot_height
+        xmin, xmax = self.figure.x_range.start, self.figure.x_range.end
+        ymin, ymax = self.figure.y_range.start, self.figure.y_range.end
+        x_range = (xmin, xmax)
+        y_range = (ymin, ymax)
+        dw, dh = xmax - xmin, ymax - ymin
+        cvs = ds.Canvas(
+            plot_width=plot_width,
+            plot_height=plot_height,
+            x_range=x_range,
+            y_range=y_range,
+        )
+        agg = cvs.line(self.lines,
+                       x=['x0','x1'], y=['y0','y1'],
+                       agg=ds.count_cat('c'), axis=1,
+        )
+        image = tf.shade(agg,
+                         min_alpha=255,
+                         color_key=self.color_key
+        )
+        return image
 
     """ Callback in FIGURE_RANGE_JSCODE """
     def figure_range(self, ax=None, start=None, end=None):
@@ -55,6 +106,16 @@ class BokehRenderer(object):
         # TODO:
         print(self.figure.x_range.start, self.figure.x_range.end)
         print(self.figure.y_range.start, self.figure.y_range.end)
+        xmin, xmax = self.figure.x_range.start, self.figure.x_range.end
+        ymin, ymax = self.figure.y_range.start, self.figure.y_range.end
+        x_range = (xmin, xmax)
+        y_range = (ymin, ymax)
+        dw, dh = xmax - xmin, ymax - ymin
+        image = self.updateImage()
+        self.source.data.update(
+            dict(image=[image.data], x=[x_range[0]],
+                        y=[y_range[0]], dw=[dw], dh=[dh])
+        )
         push_notebook(handle=self.notebook_handle)
         pass
 
