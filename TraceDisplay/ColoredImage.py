51250f7101dbddaab28a2c350013f2c1e9d889f3
Damien Carver <carverdamien@gmail.com>
2020-02-05 11:41:56 +0100

    ColoredImage: mv color value in a value field

--- TraceDisplay/ColoredImage.py
+++ TraceDisplay/ColoredImage.py
@@ -7,27 +7,27 @@ class ColoredImage(Image):
     def __init__(self, image, inplace=True, *args, **kwargs):
         super(ColoredImage, self).__init__(*args, **kwargs)
         # From https://repository.lboro.ac.uk/articles/Categorical_colormap_optimization_with_visualization_case_studies/9401987/files/17018552.pdf
-        self.color_map = {
-            0  : '#660066',
-            1  : '#006633',
-            2  : '#FFCC00',
-            3  : '#CC3333',
-            4  : '#996633',
-            5  : '#000099',
-            6  : '#000000',
-            7  : '#0099CC',
-            8  : '#868F98',
-            9  : '#009999',
-            10 : '#CC9999',
-            11 : '#66CCCC',
-            12 : '#66CC00',
-            13 : '#FF6600',
+        self.color = {
+            0  : {'value':'#660066'},
+            1  : {'value':'#006633'},
+            2  : {'value':'#FFCC00'},
+            3  : {'value':'#CC3333'},
+            4  : {'value':'#996633'},
+            5  : {'value':'#000099'},
+            6  : {'value':'#000000'},
+            7  : {'value':'#0099CC'},
+            8  : {'value':'#868F98'},
+            9  : {'value':'#009999'},
+            10 : {'value':'#CC9999'},
+            11 : {'value':'#66CCCC'},
+            12 : {'value':'#66CC00'},
+            13 : {'value':'#FF6600'},
         }
         if inplace:
             df = image.df
         else:
             df = image.df.copy()
-        color = itertools.cycle(list(self.color_map.keys()))
+        color = itertools.cycle(list(self.color.keys()))
         for k in df:
             assert 'color' not in df[k]
             df[k]['color'] = next(color)
@@ -42,6 +42,6 @@ class ColoredImage(Image):
             return df.drop(columns=todrop)
         line = pd.concat([drop(self.df[k]) for k in self.df])
         line['color'] = line['color'].astype('category')
-        color_key = [self.color_map[color] for color in np.unique(line['color'])]
+        color_key = [self.color[color]['value'] for color in np.unique(line['color'])]
         self.line_and_color_key = line, color_key
         return self.line_and_color_key
