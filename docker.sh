fef948ff46443d4294435fd70a60178c535ed4a1
Damien Carver <carverdamien@gmail.com>
2020-02-14 11:51:27 +0100

    docker.sh: deploy neo4j and link it in tracedisplay

--- docker.sh
+++ docker.sh
@@ -2,7 +2,15 @@
 set -e -x
 img=tracedisplay
 docker build -t ${img} .
-docker run --rm \
+# Set PUBLISH_NEO4J=' ' to avoid publishing ports
+: ${PUBLISH_NEO4J:=-p 7474:7474 -p 7687:7687}
+docker run --name neo4j \
+       --rm -d \
+       ${PUBLISH_NEO4J} \
+       neo4j
+docker run --name ${img} \
+       --link neo4j \
+       --rm -d \
        -v ${ROOT:=$PWD}/TraceDisplay/examples:/home/server/TraceDisplay/examples:ro \
        -v ${ROOT:=$PWD}/TraceDisplay/examples/notebook:/home/server/TraceDisplay/examples/notebook \
        -p ${PORT:=443}:443 \
