3bdc9a07a128d5b5b912f93d90cffefdee659d83
Damien Carver <carverdamien@gmail.com>
2020-03-18 13:32:03 +0100

    fix

--- TraceDisplay/__main__.py
+++ TraceDisplay/__main__.py
@@ -12,7 +12,7 @@ def find(walk_path, path_match='.*'):
 class TestTrace(unittest.TestCase):
     def test_load_and_save(self):
         count = 0
-        for trace_path in find('.', '.*.dat'):
+        for trace_path in find('.', '.*.dat$'):
             hdf_path = os.path.splitext(trace_path)[1] + '.h5'
             t = Trace()
             t.load(trace_path)
