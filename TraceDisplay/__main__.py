315457d836621db3c690fa63ff81ffbe494683db
Damien Carver <carverdamien@gmail.com>
2020-03-18 12:58:08 +0100

    test

--- TraceDisplay/__main__.py
+++ TraceDisplay/__main__.py
@@ -1,4 +1,5 @@
 import unittest, os, re
+import TraceDisplay
 
 def find(walk_path, path_match='.*'):
     match = re.compile(path_match)
@@ -10,11 +11,14 @@ def find(walk_path, path_match='.*'):
 
 class TestTrace(unittest.TestCase):
     def test_load_and_save(self):
+        count = 0
         for trace_path in find('.', '.*.dat'):
             hdf_path = os.path.splitext(trace_path)[1] + '.h5'
-            t = Trace()
+            t = TraceDisplay.Trace()
             t.load(trace_path)
             t.save(hdf_path)
+            count += 1
+        self.assertTrue(count>0)
 
 if __name__ == '__main__':
     unittest.main()
