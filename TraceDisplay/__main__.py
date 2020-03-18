c61bb0592f6bb5c35d66f075afd1f730ef77411d
Damien Carver <carverdamien@gmail.com>
2020-03-18 12:26:31 +0100

    test_load_and_save

--- TraceDisplay/__main__.py
+++ TraceDisplay/__main__.py
@@ -1,4 +1,20 @@
-import unittest
+import unittest, os, re
+
+def find(walk_path, path_match='.*'):
+    match = re.compile(path_match)
+    for dirpath, dirnames, filenames in os.walk(walk_path):
+        for filename in filenames:
+            to_match = os.path.join(dirpath, filename)
+            if match.match(to_match):
+                yield to_match
+
+class TestTrace(unittest.TestCase):
+    def test_load_and_save(self):
+        for trace_path in find('.', '.*.dat'):
+            hdf_path = os.path.splitext(trace_path)[1] + '.h5'
+            t = Trace()
+            t.load(trace_path)
+            t.save(hdf_path)
 
 if __name__ == '__main__':
     unittest.main()
