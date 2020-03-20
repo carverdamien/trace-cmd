cfdb5fd64456ce55f40ec1b02f2cca1acf5a315e
Damien Carver <carverdamien@gmail.com>
2020-03-20 11:36:00 +0100

    add TestDataFrameCollection

--- TraceDisplay/__main__.py
+++ TraceDisplay/__main__.py
@@ -1,7 +1,11 @@
 import unittest, os, re, logging
+import numpy as np
 from Trace import Trace
 from Image import Image
 from Render import render
+from DataFrameCollection import DataFrameCollection
+
+NR_SCALE = 10
 
 def find(walk_path, path_match='.*'):
     match = re.compile(path_match)
@@ -31,6 +35,45 @@ class TestTrace(unittest.TestCase):
             render(render_path, img_path)
             count += 1
         self.assertTrue(count>0)
+
+def randomDataFrame(nr_rows=NR_SCALE, nr_cols=NR_SCALE, dtype=[int, float, str]):
+    return pd.DataFrame({
+        'col%d' % (c) : np.array(np.random.rand(nr_rows), dtype=dtype[np.random.randint(len(dtype))])
+        for c in range(nr_cols)
+    })
+
+def randomDictOfDataFrame(nr_keys=NR_SCALE, nr_rows=NR_SCALE, nr_cols=NR_SCALE):
+    return {
+        'key%d' : randomDataFrame(nr_rows, nr_col)
+        for k in range(nr_keys)
+    }
+
+class TestDataFrameCollection(unittest.TestCase):
+    def test_init(self):
+        DataFrameCollection(randomDictOfDataFrame())
+        pass
+    def test_set_get(self):
+        dfc = DataFrameCollection()
+        for k,v in randomDictOfDataFrame():
+            dfc[k] = v
+            assert v is dfc[k]
+        pass
+    def test_set_fails_if_not_DataFrame(self):
+        dfc = DataFrameCollection()
+        def fails():
+            dfc['foo'] = {}
+        self.assertRaises(Exception, fails)
+        pass
+    def test_private_keys(self):
+        i = Image()
+        for k in Image.PRIVATE_KEYS:
+            def set_fails():
+                i[k] = randomDataFrame()
+            self.assertRaises(Exception, set_fails)
+            def get_fails():
+                foo = i[k]
+            self.assertRaises(Exception, get_fails)
+        pass
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
