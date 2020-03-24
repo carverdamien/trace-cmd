937652948af4fcffbe9d66bf1b43fab195d084da
Damien Carver <carverdamien@gmail.com>
2020-03-24 17:11:55 +0100

    dont use %r

--- TraceDisplay/Renderer/BokehRenderer.py
+++ TraceDisplay/Renderer/BokehRenderer.py
@@ -12,8 +12,8 @@ output_notebook()
 
 BOKEH_RENDERER = {}
 FIGURE_RANGE_JSCODE = """
-var brid = "%r";
-var ax   = "%r";
+var brid = "%s";
+var ax   = "%s";
 var notebook = Jupyter.notebook;
 var start = cb_obj.start;
 var end = cb_obj.end;
