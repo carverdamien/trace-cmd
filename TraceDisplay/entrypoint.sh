fbe5828ca9c1929ac73ad64707c29237b5cad930
Damien Carver <carverdamien@gmail.com>
2020-04-09 16:45:33 +0200

    do not overwrite existing mykey.key mycert.pem

--- TraceDisplay/entrypoint.sh
+++ TraceDisplay/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+([ -f mykey.key ] && [ -f mycert.pem ]) ||
 while echo ; do :;done | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
 jupyter-notebook --allow-root --port=443 --ip=0.0.0.0 --certfile=mycert.pem --keyfile=mykey.key
