a6208bedc75a86fda12745160b32dde7247845af
Damien Carver <carverdamien@gmail.com>
2020-04-14 18:50:19 +0200

    mv Render in scripts

--- TraceDisplay/TraceDisplay/__init__.py
+++ TraceDisplay/TraceDisplay/__init__.py
@@ -2,4 +2,3 @@ from .DataFrameCollection import DataFrameCollection
 from .Trace import Trace
 from .Image import Image
 from .BokehRenderer import BokehRenderer, BOKEH_RENDERER
-from .Render import bokeh_render, plotly_render, mpl_render
